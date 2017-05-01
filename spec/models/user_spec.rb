0a90ddc9cc04350815461e458318df8b8405695e
Jonathan Chang <jonoschang7@gmail.com>
2017-04-30 23:23:34 -0400

    add association and validation tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with Todo model
+  it { should have_many(:todos) }
+
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
