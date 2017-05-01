795cff052adf65d989238f806115d078c67531bf
Jonathan Chang <jonoschang7@gmail.com>
2017-05-01 01:20:32 -0400

    decode encode for JWT

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,21 @@
+class JsonWebToken
+	#secret to encode and decode token
+	HMAC_SECRET = Rails.applciation.secrets.secret_key_base
+
+	def self.encode(payload, exp = 24.hours.from.now)
+		# set expiry to 24 hours from creation time
+		payload[:exp] = exp.to_i
+
+		# sign token with application secret
+		JWT.encode(payload, HMAC_SECRET)
+	end
+
+	def self.decode(token)
+		body = JWT.decode(token, HMAC_SECRET)[0]
+		HashWithIndifferentAccess.new body
+		# rescue from expiry exception
+	rescue JWT::ExpiredSignature, JWT::VerificationError => e
+		# raise custom error to be handled by custom handler
+		raise ExceptionHandler::ExpiredSignature, e.message
+	end
+end
